63dc9e570e72c172d1188ee7a811a8ccfd1e5158
BillDorn <billdorn@lawn-128-61-54-26.lawn.gatech.edu>
2013-10-26 03:53:43 -0400

    Added detector helper function file
    Added obstacle detection functions including graham scan.

---
+++ obstacle_detector/src/detector_functions.py
@@ -0,0 +1,41 @@
+"""Provides functions for use in the obstacle detection node."""
+
+def polar2cart(coords):
+    """
+    Convert list of polar coordinates to cartesian.
+
+    Args:
+    coords -- iterable of polar points in form [(rho,phi),...]
+    """
+    pass
+
+def _ccw(p1, p2, p3):
+    """
+    Return direction of vector P1P2 X P1P3.
+
+    Args:
+    p1,p2,p3 -- 2D points represented as tuples (x,y)
+    """
+    return (p2[0]-p1[0])*(p3[1]-p1[1]) - (p2[1]-p1[1])*(p3[0]-p1[0])
+
+
+def graham_scan(points):
+    """
+    Return convex hull.
+
+    Args:
+    points -- iterable of 2D points in form [(x,y),...]
+    """
+    upper_hull = []
+    lower_hull = []
+    points = sorted(points)
+    for p in points:
+        while len(upper_hull) > 1 and _ccw(upper_hull[-2], upper_hull[-1], p) <= 0:
+            upper_hull.pop()
+        while len(lower_hull) > 1 and _ccw(lower_hull[-2], lower_hull[-1], p) >= 0:
+            lower_hull.pop()
+        upper_hull.append(p)
+        lower_hull.append(p)
+    return upper_hull + lower_hull[-2:0:-1]
+        
+        
