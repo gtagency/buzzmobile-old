7dfa02d8ab53bfc18366989dfd8f9e49660b4bba
billdorn <dorn.bill@gmail.com>
2013-10-30 17:46:17 -0400

    Modified c code.
    Fixed function signature. Added num_edge variable to let python quickly
    create the resultant edge list.

--- obstacle_detector/src/detector_lib.c
+++ obstacle_detector/src/detector_lib.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
-void get_edges(double *x1, double *y1,
-               double *x2, double *y2,
-               double *x1out, double *y1out,
-               double *x2out, double *y2out,
-               int length);
+void get_edges(double *points_x, double *points_y,
+               double *edges1_x, double *edges1_y,
+               double *edges2_x, double *edges2_y,
+               int length, int *num_edges);
 
-void get_edges(double *x1, double *y1,
-               double *x2, double *y2,
-               double *x1out, double *y1out,
-               double *x2out, double *y2out,
-               int length){
+void get_edges(double *points_x, double *points_y,
+               double *edges1_x, double *edges1_y,
+               double *edges2_x, double *edges2_y,
+               int length, int *num_edges){
     int i,j;
     int n = 0;
-    double d = 1;
+    double d, dx, dy;
+    d = 1;
 
     for (i = 0; i < length; i++){
         for (j = i+1; j < length; j++){
-            if(((x2[i]-x1[i])*(x2[i]-x1[i]) + (y2[i]-y1[i])*(y2[i]-y1[i])) <= d){
-                x1out[n] = x1[i];
-                y1out[n] = y1[j];
-                x2out[n] = x2[i];
-                y2out[n] = y2[j];
+            dx = points_x[j] - points_x[i];
+            dy = points_y[j] - points_y[i];
+            if((dx*dx) + (dy*dy) <= d){
+                edges1_x[n] = points_x[i];
+                edges1_y[n] = points_y[i];
+                edges2_x[n] = points_x[j];
+                edges2_y[n] = points_y[j];
                 n++;
             }
         }
     }
+    *num_edges = n;
 }
