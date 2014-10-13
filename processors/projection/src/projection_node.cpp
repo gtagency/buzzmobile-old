6e131e63a6b1b89de82cfb6d37cc312539f83d8e
Jesse Rosalia <jesse.rosalia@gatech.edu>
2014-10-12 20:36:46 -0700

    moved params out to ros param objects

--- processors/projection/src/projection_node.cpp
+++ processors/projection/src/projection_node.cpp
@@ -24,14 +24,14 @@ const std::string& getRosType(int cvType) {
 }
 
 //NOTE: values copied from Nick's original code, hand tuned to Eleanor.
-proj::ProjectionParams params(800,800,120.0,120.0,21.0/12.0,1000.0,M_PI_2+.311);
+proj::ProjectionParams *params = NULL;
 void imageCallback(const sensor_msgs::Image::ConstPtr& image) {
   cv_bridge::CvImagePtr cv_ptr;
   try {
-//    if (enc::isColor(image->encoding))
-//      cv_ptr = cv_bridge::toCvShare(image, enc::BGR8);
-//    else
-//      cv_ptr = cv_bridge::toCvShare(image, enc::MONO8);
+/*    if (enc::isColor(image->encoding))
+      cv_ptr = cv_bridge::toCvShare(image, enc::BGR8);
+    else
+      cv_ptr = cv_bridge::toCvShare(image, enc::MONO8);*/
     cv_ptr = cv_bridge::toCvCopy(image, enc::BGR8);
   }
   catch (cv_bridge::Exception& e) {
@@ -40,10 +40,8 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& image) {
   }
 
   cv::Mat proj;
-  proj::groundTransformProj(cv_ptr->image, params, proj);
+  proj::groundTransformProj(cv_ptr->image, *params, proj);
   balance::grayWorld(proj, proj);
-
-
 
   std_msgs::Header header;
   cv_bridge::CvImage projImage(header, getRosType(proj.type()), proj);
@@ -51,9 +49,32 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& image) {
   proj_pub.publish(projImage.toImageMsg());
 }
 
+#define DECLARE_PARAM(name, type, defVal)\
+  type name;\
+  if (!n.getParam(#name, name)) {\
+    name = defVal;\
+  }
+
+proj::ProjectionParams getParams() {
+
+  ros::NodeHandle n("~");
+  DECLARE_PARAM(output_x_res, int, 800);
+  DECLARE_PARAM(output_y_res, int, 800);
+  DECLARE_PARAM(ground_x_dim, double, 120.0);
+  DECLARE_PARAM(ground_y_dim, double, 120.0);
+  DECLARE_PARAM(ground_z_dim, double, 21.0/12.0);
+  DECLARE_PARAM(camera_scale, double, 1000.0);
+  DECLARE_PARAM(camera_pitch, double, M_PI_2);
+
+  return proj::ProjectionParams(output_x_res,output_y_res,ground_x_dim,ground_y_dim,ground_z_dim,camera_scale,camera_pitch);
+}
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "projection");
   ros::NodeHandle n;
+
+  proj::ProjectionParams localParams = getParams();
+  params = &localParams;
   ros::Subscriber sub = n.subscribe<sensor_msgs::Image>("image_raw", 1000, imageCallback);
   proj_pub = n.advertise<sensor_msgs::Image>("image_projected", 100);
 
