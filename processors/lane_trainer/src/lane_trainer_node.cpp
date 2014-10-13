37558b5e3c1deb98acbafaa23ab7f74454593956
Jesse Rosalia <jesse.rosalia@gatech.edu>
2014-10-12 20:36:07 -0700

    modified to work with green strips (for testing), and fixed bugs/issues with pulling out the right strip of road
    code could stand to be a lot cleaner

--- processors/lane_trainer/src/lane_trainer_node.cpp
+++ processors/lane_trainer/src/lane_trainer_node.cpp
@@ -9,13 +9,15 @@
 
 ros::Publisher pub;
 
-void imageMsgToCvCopy(const sensor_msgs::Image::ConstPtr& image, cv_bridge::CvImagePtr& cv_ptr) {
+namespace enc = sensor_msgs::image_encodings;
+
+void imageMsgToCvCopy(const sensor_msgs::Image::ConstPtr& image, cv_bridge::CvImageConstPtr& cv_ptr) {
   try {
-//    if (enc::isColor(image->encoding))
-//      cv_ptr = cv_bridge::toCvShare(image, enc::BGR8);
-//    else
-//      cv_ptr = cv_bridge::toCvShare(image, enc::MONO8);
-    cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+    if (enc::isColor(image->encoding))
+      cv_ptr = cv_bridge::toCvShare(image, enc::BGR8);
+    else
+      cv_ptr = cv_bridge::toCvShare(image, enc::MONO8);
+//    cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
   }
   catch (cv_bridge::Exception& e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
@@ -23,25 +25,47 @@ void imageMsgToCvCopy(const sensor_msgs::Image::ConstPtr& image, cv_bridge::CvIm
   }
 }
 
+struct Vec3bCompare {
+public:
+  bool operator()(const cv::Vec3b &a, const cv::Vec3b &b) const {
+    return a[0] < b[0] && a[1] < b[1] && a[2] < b[2];
+  }
+};
+
+void addPointsToInstanceArray(const std::set<cv::Vec3b,Vec3bCompare>& pts, int label, lane_trainer::LaneInstanceArray& lanes) {
+  for (std::set<cv::Vec3b>::iterator it = pts.begin();
+       it != pts.end();
+       ++it) {
+    lane_trainer::LaneInstance lane;
+    lane.h = it->val[0];
+    lane.s = it->val[1];
+    lane.label = label;
+    lanes.instanceArray.push_back(lane);
+  }
+}
+
+#define YELLOW_LOW  cv::Scalar(0,150,200)
+#define YELLOW_HIGH cv::Scalar(120,255,255)
+#define GREEN_LOW   cv::Scalar(70,80,40)
+#define GREEN_HIGH  cv::Scalar(156,168,117)
 void generateTrainingSet(const sensor_msgs::Image::ConstPtr& image) {
 
-  std::cout << "Image received" << std::endl;
+ //std::cout << "Image received" << std::endl;
   cv::Mat projectedImage, sobelImg, threshImg;
-  const int SLICE_Y = 2*(projectedImage.rows/3);
 
-  cv_bridge::CvImagePtr cv_ptr;
+  cv_bridge::CvImageConstPtr cv_ptr;
   imageMsgToCvCopy(image, cv_ptr); 
   projectedImage = cv_ptr->image;
 
+  const int SLICE_Y = 1*(projectedImage.rows/6);
   cv::GaussianBlur(projectedImage, projectedImage, cv::Size(0, 0), 1, 0);
 
-  //cv::cvtColor(projectedImage, projectedImage, CV_BGR2HSV);
   cv::Mat slice(projectedImage, cv::Rect(0, SLICE_Y, projectedImage.cols, 1));
   
   cv::Mat HSVSlice;
   cv::cvtColor(slice, HSVSlice, CV_BGR2HSV);
 
-  cv::inRange(slice, cv::Scalar(0, 150, 200), cv::Scalar(120, 255, 255), threshImg);
+  cv::inRange(slice, GREEN_LOW, GREEN_HIGH, threshImg);
   //cv::inRange(slice, cv::Scalar(45, 255, 0), cv::Scalar(60, 135, 255), threshImg);
 
   cv::Sobel(slice, sobelImg, -1, 1, 0);
@@ -50,60 +74,78 @@ void generateTrainingSet(const sensor_msgs::Image::ConstPtr& image) {
 
   //cv::imshow("Sobel", sobelImg);
 
-  int center = -1;
-  std::vector<cv::Vec3b> linePts;
+  int leftEdge = -1;
+  int rightEdge = -1;
+  std::set<cv::Vec3b, Vec3bCompare> linePts;
+  std::set<cv::Vec3b, Vec3bCompare> notRoadPts;
+  std::set<cv::Vec3b, Vec3bCompare> roadPts;
 
-  for (int i = slice.cols-1; i >= 0; --i) {
+//  for (int i = leftEdge; i < slice.cols; ++i) {
+  //
+  int center = 2*slice.cols /3;
+  for (int i = center; i >= 0; --i) {
     if (threshImg.at<uchar>(0, i) == 255) {
-      linePts.push_back(HSVSlice.at<cv::Vec3b>(0, i));
-      if (center == -1) {
-	center = i;
-      }
+  	  leftEdge = i;
+    } else if (leftEdge != -1) {
+      break;
+    }
+  }
+  for (int i = leftEdge; i < slice.cols; ++i) {
+    if (sobelImg.at<uchar>(0, i) == 255 || threshImg.at<uchar>(0, i) == 255) {
+      linePts.insert(HSVSlice.at<cv::Vec3b>(0, i));
+      cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(0, 255, 0), -1);
+  	  leftEdge = i;
+    } else {
+      break;
     }
   }
 
-  //std::cout << center << std::endl;
-  //std::cout << slice.cols/2 << std::endl;
-
-  std::vector<cv::Vec3b> roadPts;
+  for (int i = leftEdge + 1; i < slice.cols; ++i) {
+    if (sobelImg.at<uchar>(0, i) == 255 || threshImg.at<uchar>(0, i) == 255) {
+      cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(0, 255, 0), -1);
+      rightEdge = i;
+    } else {
+      if (rightEdge == -1) {
+        roadPts.insert(HSVSlice.at<cv::Vec3b>(0, i));
+      	cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(255, 0, 0), -1);
+      } else {
+        notRoadPts.insert(HSVSlice.at<cv::Vec3b>(0, i));
+      	cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(0, 0, 255), -1);
+      }
+    }
+  }
+  std::cout << center << std::endl;
+  std::cout << leftEdge << std::endl;
+  std::cout << rightEdge << std::endl;
+  std::cout << slice.cols/2 << std::endl;
+/*
   bool stop = false;
   for (int i = center + 2; i < slice.cols; ++i) {
     if(!stop) {
       if(sobelImg.at<uchar>(0, i) < 200) {
-	roadPts.push_back(HSVSlice.at<cv::Vec3b>(0, i));
-	cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(255, 0, 0), -1);
+	      roadPts.insert(HSVSlice.at<cv::Vec3b>(0, i));
+      	cv::circle(projectedImage, cv::Point(i, SLICE_Y), 1, cv::Scalar(255, 0, 0), -1);
       } else {
-	stop = true;
+      	stop = true;
       }
     }
   }
+*/
 
   lane_trainer::LaneInstanceArray lanes;
-  for (uint i = 0; i < roadPts.size(); ++i) {
-    lane_trainer::LaneInstance lane;
-    lane.h = roadPts.at(i).val[0];
-    lane.s = roadPts.at(i).val[1];
-    lane.label = 1;
-    lanes.instanceArray.push_back(lane);
-  }
-
-  for (uint i = 0; i < linePts.size(); ++i) {
-    lane_trainer::LaneInstance lane;
-    lane.h = roadPts.at(i).val[0];
-    lane.s = roadPts.at(i).val[1];
-    lane.label = 2;
-    lanes.instanceArray.push_back(lane);
-  }
+  addPointsToInstanceArray(notRoadPts, 0, lanes);
+  addPointsToInstanceArray(roadPts,    1, lanes);
+  addPointsToInstanceArray(linePts,    2, lanes);
 
   pub.publish(lanes);
 
   //std::cout << roadPts.size() << std::endl;
 
-  //cv::imshow("Projection", threshImg);
-  //cv::imshow("Yellow Line", yellowLine);
-  //cv::imshow("Result", projectedImage);
+  cv::imshow("threshImg", threshImg);
+  cv::imshow("sobelImg", sobelImg);
   
-  //cv::waitKey();
+  cv::imshow("projectedImage", projectedImage);
+  cv::waitKey();
 }
 
 int main(int argc, char *argv[]) {
