bea68ce34d1e18e0b914c193fbb3e2eb125c5dae
Jesse Rosalia <jesse.rosalia@gatech.edu>
2014-10-26 12:08:30 -0400

    moved BicycleModelCalculations into a ros library package started port to c++

---
+++ library/bicycle_model/src/bicycle_model_calculations.cpp
@@ -0,0 +1,25 @@
+
+#include "bicycle_model_calculations.h"
+
+geometry_msgs::Vector3 changePosition(double linearVel, double steerAngle, double timeSecs, double carLength) {
+  return forwardKinematics(linearVel, angularVelocity(linearVel, steerAngle, carLength), timeSecs);
+}
+
+
+geometry_msgs::Vector3 forwardKinematics(double linearVel, double angularVel, double timeSecs) {
+  double dth = angularVel * timeSecs;
+  double dx  = linearVel * sin(dth) / angularVel;
+  double dy  = linearVel * (1 - cos(dth)) / angularVel;
+
+  geometry_msgs::Vector3 msg;
+  msg.x = dx;
+  msg.y = dy;
+  msg.z = dth;
+  return msg;
+}
+
+double angularVelocity(double linearVel, double steerAngle, double carLength) {
+  return linearVel * tan(steerAngle) / carLength;
+}
+
+
