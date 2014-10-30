7d6807498f13cc1bd5f2a13558c14df6fdfbf933
Daniel Cohen <dcohen@gatech.edu>
2014-10-29 18:30:07 -0700

    Fixed gate detector code

--- processors/gate_detector/src/gate_detector_node.cpp
+++ processors/gate_detector/src/gate_detector_node.cpp
@@ -13,6 +13,8 @@
 #include <core_msgs/Obstacle.h>
 #include <core_msgs/WorldRegion.h>
 
+#define MINIMUM_DISTANCE_METERS 2.5
+
 ros::Publisher region_pub;
 
 struct Params {
@@ -24,8 +26,12 @@ struct Params {
 
 Params params;
 
-bool isYNegative(const core_msgs::Obstacle& ob) {
-  return ob.center.y < 0;
+bool isYPositive(const core_msgs::Obstacle& ob) {
+  return ob.center.y > 0;
+}
+
+int line(const geometry_msgs::Point32& a, const geometry_msgs::Point32& b, int x) {
+  return a.y + (((b.y - a.y)/(double) (b.x - a.x)) * (x - a.x));
 }
 
 //distance from 0,0 = magnitude of point vector
@@ -39,21 +45,43 @@ bool obsDistanceCompare(const core_msgs::Obstacle& ob1, const core_msgs::Obstacl
 
 void obstaclesCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& inmsg) {
   std::vector<core_msgs::Obstacle> obstacles(inmsg->obstacles);
-  std::vector<core_msgs::Obstacle>::iterator bound = std::partition(obstacles.begin(), obstacles.end(), isYNegative);
-
+  std::vector<core_msgs::Obstacle>::iterator bound = std::partition(obstacles.begin(), obstacles.end(), isYPositive);
   core_msgs::Obstacle post1 = *std::max_element(obstacles.begin(), bound, obsDistanceCompare);
   core_msgs::Obstacle post2 = *std::max_element(bound, obstacles.end(), obsDistanceCompare);
   core_msgs::WorldRegion msg;
   msg.width = params.output_x_res;
   msg.height = params.output_y_res;
   msg.resolution = params.pixels_per_meter;
-  msg.labels.reserve(msg.width * msg.height);
+  msg.labels.assign(msg.width * msg.height, 0);
+  geometry_msgs::Point32 bottomLeft, bottomRight;
+  bottomRight.y = -(params.car_width_meters / 2) * msg.resolution;
+  bottomLeft.y = (params.car_width_meters / 2) * msg.resolution;
+  bottomLeft.x = bottomRight.x = 0;
+
+  post1.center.x *= msg.resolution;
+  post1.center.y *= msg.resolution;
+
+  post2.center.x *= msg.resolution;
+  post2.center.y *= msg.resolution;
+
   for (int row = 0; row < params.output_y_res; row++) {
     for (int col = 0; col < params.output_x_res; col++) {
-      //TODO: draw the region HERE
-      // msg.labels[row * msg.width + col] = 0 or 1 (1 is drivable)
+      int position = row * msg.width + col;
+      int x = msg.height - row;
+      // This assumes that y is positive to the right. Might want to fix in obstacle detector
+      int y = col - (msg.width / 2);
+      if (x > MINIMUM_DISTANCE_METERS / msg.resolution) {
+	if (x > line(bottomLeft, post2.center, y) && x > line(bottomRight, post1.center, y)) {
+	  msg.labels[position] = 1;
+	} else {
+	  msg.labels[position] = 0;
+	}
+       } else {
+	  msg.labels[position] = 0;
+       }
     }
   }
+  region_pub.publish(msg);
 }
 
 #define DECLARE_PARAM(name, type, defVal)\
@@ -70,12 +98,13 @@ int main(int argv, char **argc) {
   DECLARE_PARAM(car_width, double, 5);
   DECLARE_PARAM(output_x_res, double, 800);
   DECLARE_PARAM(output_y_res, double, 800);
-  DECLARE_PARAM(pixels_per_meter, double, 40);
+  //Was 40 pixels/meter
+  DECLARE_PARAM(pixels_per_meter, double, 10);
 
   params.car_width_meters = car_width;
   params.output_x_res = output_x_res;
   params.output_y_res = output_y_res;
-  params.pixels_per_meter= pixels_per_meter;
+  params.pixels_per_meter = pixels_per_meter;
 
   ros::Subscriber s = n.subscribe<core_msgs::ObstacleArrayStamped>("/obstacles", 100, obstaclesCallback);
   //TODO: occupancy map?
