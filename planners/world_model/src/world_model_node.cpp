b6287f2bafe5efc2123d77764b3305b65b1ca0bd
Jesse Rosalia <jesse.rosalia@gatech.edu>
2014-10-29 23:04:56 -0400

    attempt to fix units, and instead making things worse.  passing the batton...

--- planners/world_model/src/world_model_node.cpp
+++ planners/world_model/src/world_model_node.cpp
@@ -13,15 +13,28 @@ struct RegionCallback {
   core_msgs::WorldRegion region;
   void operator()(const core_msgs::WorldRegion::ConstPtr& region) {
     this->region = *region;
+    std::cout << "Receivin: " << this->region.width << std::endl;
     updateWorldModel();
   }
 };
 
-RegionCallback gateRegionCallback;
-RegionCallback laneRegionCallback;
+core_msgs::WorldRegion gateRegion;
+
+void gateRegionCallback(const core_msgs::WorldRegion::ConstPtr& region) {
+  gateRegion = *region;
+  updateWorldModel();
+}
+core_msgs::WorldRegion laneRegion;
+
+void laneRegionCallback(const core_msgs::WorldRegion::ConstPtr& region) {
+  laneRegion = *region;
+  updateWorldModel();
+}
+
 std::vector<core_msgs::Obstacle> obstacles;
 void obstaclesCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& obsArray) {
   obstacles = obsArray->obstacles;
+  std::cout << obstacles.size() << std::endl;
   updateWorldModel();
 }
 
@@ -30,29 +43,40 @@ void updateWorldModel() {
   //TODO: this also ignores timestamps, assuming that we're just going to get a lot of data...
   //
   // First: construct a region wherein everything is drivable except the obstacles
+  std::cout << "Updating" << laneRegion.width << std::endl;
   core_msgs::WorldRegion obsregion;
-  obsregion.width = gateRegionCallback.region.width;
-  obsregion.height = gateRegionCallback.region.height;
-  obsregion.resolution = gateRegionCallback.region.resolution;
+  core_msgs::WorldRegion& baseRegion = laneRegion;
+  if (baseRegion.width == 0) {
+    return;
+  }
+  std::cout << "I'm in" << std::endl;
+  obsregion.width = baseRegion.width;
+  obsregion.height = baseRegion.height;
+  obsregion.resolution = 100; // baseRegion.resolution;
+  int yres = 40;
   obsregion.labels.assign(obsregion.width * obsregion.height, 1); //assume everything is drivable
   for (std::vector<core_msgs::Obstacle>::iterator it = obstacles.begin();
        it != obstacles.end();
        it++) {
-    int centerRow = obsregion.height - (it->center.x * obsregion.resolution); 
-    int centerCol = obsregion.width - (it->center.y * obsregion.resolution);
+    //FIXME: NOT SURE IF RIGHT THIS CODE IS TERRIBLE PLEASE DONT JUDGE ME
+    unsigned int centerRow = obsregion.height - (it->center.x * obsregion.resolution); 
+    unsigned int centerCol = obsregion.width / 2 + (it->center.y * yres);
     int radiusInPixels = (int)ceil(it->radius * obsregion.resolution);
-    for (int row = centerRow - radiusInPixels; row <= centerRow + radiusInPixels; row++) {
+    std::cout << it->center.x << "," << centerRow << "," << it->center.y << "," << centerCol << "," << radiusInPixels << std::endl;
+    
+    for (unsigned int row = std::min(obsregion.height - 1, centerRow + radiusInPixels); row <= std::max(0, (int)(centerRow - radiusInPixels)); --row) {
       int colDist = (int)ceil(sqrt(radiusInPixels * radiusInPixels - row * row));
-      for (int col = centerCol - colDist; col <= centerCol + colDist; col++) {
+      for (unsigned int col = std::max((unsigned int)0, centerCol - colDist); col <= std::min(obsregion.width - 1, centerCol + colDist); col++) {
+        std::cout << row << "," << col << std::endl;
         obsregion.labels[row * obsregion.width + col] = 0;
       }
     }
   }
 
   // Next: merge all 3 regions together
-  core_msgs::WorldRegion merged = gateRegionCallback.region;
+  core_msgs::WorldRegion merged = laneRegion; //gateRegionCallback.region;
   for (unsigned int inx = 0; inx < merged.width * merged.height; inx++) {
-    if (merged.labels[inx] != laneRegionCallback.region.labels[inx]
+    if (merged.labels[inx] != laneRegion.labels[inx]
         || merged.labels[inx] != obsregion.labels[inx]) {
       merged.labels[inx] = 0;
     }
