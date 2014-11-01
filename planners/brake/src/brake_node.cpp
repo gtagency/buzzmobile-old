882fbe37da5b69ff35502a702ed81b41a1c90903
Jesse Rosalia <jesse.rosalia@gatech.edu>
2014-10-31 23:14:06 -0400

    fixed compiler errors, small refactor

--- planners/brake/src/brake_node.cpp
+++ planners/brake/src/brake_node.cpp
@@ -2,18 +2,18 @@
 #include <ros/ros.h>
 #include <vector>
 #include <std_msgs/Bool.h>
-#include <geometry_msgs/Point32.h>
-#include <geometry_msgs/Polygon.h>
+//#include <geometry_msgs/Point32.h>
+//#include <geometry_msgs/Polygon.h>
 #include <core_msgs/ObstacleArrayStamped.h>
 #include <core_msgs/Obstacle.h>
-#include <core_msgs/Pose2DAndVelStamped.h>
+//#include <core_msgs/Pose2DAndVelStamped.h>
 
-#include "bicycle_model/bicycle_model_calculations.h"
+//#include "bicycle_model/bicycle_model_calculations.h"
 
-#define CAR_WIDTH 1; //edit this
-
+#define CAR_WIDTH_METERS (1) //edit this
+#define MIN_DIST_METERS (2)
 ros::Publisher brake_pub;
-
+/*
 core_msgs::Pose2DAndVelStamped currentPoseAndVel;
 
 geometry_msgs::Point32 computeCurrentCarPoint() {
@@ -24,33 +24,32 @@ geometry_msgs::Point32 computeCurrentCarPoint() {
   pt.z = 0;
   return pt;
 }
-
+*/
 bool isCloser(const core_msgs::Obstacle& obj1, const core_msgs::Obstacle& obj2) {
   return obj1.center.y < obj2.center.y;
-
 }
 
-void obstacleInDistanceCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& msg) {
-  std::vector<core_messages::Obstacle> obstacles(msg->obstacles);
+void obstacleInDistanceCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& inmsg) {
+  std::vector<core_msgs::Obstacle> obstacles(inmsg->obstacles);
   if (obstacles.size() == 0) {
-    return false;
+    return;
   }
-  std::vector<core_messages::Obstacle> inWidth;
-  for (int i = 0; i , obstacles.size(); i++) {
-    if (obstacles[i].center.y > CAR_WIDTH || obstacles[i].center.y < (-1 * CAR_WIDTH)) {
-      inWidth.push_back(obstacles[i];
+  std::vector<core_msgs::Obstacle> inWidth;
+  for (unsigned int i = 0; i < obstacles.size(); i++) {
+    if (fabs(obstacles[i].center.y) < CAR_WIDTH_METERS) {
+      inWidth.push_back(obstacles[i]);
     }
   }
   core_msgs::Obstacle closest = *std::max_element(inWidth.begin(), inWidth.end(), isCloser);
   std_msgs::Bool msg;
-  if (closest.center.x < 2) {
+  if (closest.center.x < MIN_DIST_METERS) {
     msg.data = true;
     brake_pub.publish(msg);
   }
 }
 
 void obstacleCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& msg) {
-  geometry_msgs::Point32 testPt = computeCurrentCarPoint();  
+//  geometry_msgs::Point32 testPt = computeCurrentCarPoint();  
   /*
   for (std::vector<core_msgs::Obstacle>::const_iterator it = msg->obstacles.begin();
        it != msg->obstacles.end();
@@ -68,19 +67,19 @@ void obstacleCallback(const core_msgs::ObstacleArrayStamped::ConstPtr& msg) {
     }
   }*/
 }
-
+/*
 void carPosAndVelCallback(const core_msgs::Pose2DAndVelStamped::ConstPtr& msg) {
   currentPoseAndVel.header = msg->header;
   currentPoseAndVel.pose = msg->pose;
   currentPoseAndVel.linearVel = msg->linearVel;
 }
-
+*/
 int main(int argc, char **argv) {
 
   ros::NodeHandle n;
 
   ros::Subscriber s = n.subscribe<core_msgs::ObstacleArrayStamped>("obstacles", 100, obstacleInDistanceCallback);
-  ros::Subscriber s2 = n.subscribe<core_msgs::Pose2DAndVelStamped>("car_pos_and_vel", 100, carPosAndVelCallback);
+  //ros::Subscriber s2 = n.subscribe<core_msgs::Pose2DAndVelStamped>("car_pos_and_vel", 100, carPosAndVelCallback);
   brake_pub = n.advertise<std_msgs::Bool>("brake", 1000, true);
 
   ros::spin();
